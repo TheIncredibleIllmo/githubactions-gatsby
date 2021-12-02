e055f023250b5b129a0684276a9cc2b074c5f6eb
TheIncredibleIllmo <eduardomedm@gmail.com>
2021-12-02 09:01:33 -0600

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Hello Gatsby</title>
+      <body>
+        <h1>Hello Gatsby</h1>
+      </body>
+    </main>
+  );
+};
+
+export default IndexPage;
---
+++ test/index.test.js
@@ -0,0 +1,3 @@
+describe("Initial test", () => {
+  it("should pass", () => {});
+});
